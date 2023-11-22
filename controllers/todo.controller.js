511e05f702ce56f903614e0dd88fb307b2843be3
bluuweb <contacto@bluuweb.cl>
2023-11-22 13:23:49 -0300

    queries params

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -2,11 +2,25 @@ import { getDatabaseError } from "../lib/errors/database.error.js";
 import { todoModel } from "../models/todo.model.js";
 
 const read = async (req, res) => {
+  const { limit = 5, order = "ASC", page = 1 } = req.query;
+
+  // Utilizar una expresión regular para verificar si 'page' es un número válido
+  const isPageValid = /^[1-9]\d*$/.test(page);
+
+  // Validar el resultado de la expresión regular
+  if (!isPageValid) {
+    return res.status(400).json({ message: "Invalid page number, page > 0" });
+  }
+
   try {
-    const todos = await todoModel.findAll();
+    const todos = await todoModel.findAll({ limit, order, page });
     return res.json(todos);
   } catch (error) {
     console.log(error);
+    if (error.code) {
+      const { code, message } = getDatabaseError(error.code);
+      return res.status(code).json({ message });
+    }
     return res.status(500).json({ message: "Internal server error" });
   }
 };
